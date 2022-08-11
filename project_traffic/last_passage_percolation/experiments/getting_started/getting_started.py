bc9edc8951996741475275cb4bd21c0a4f2afe75
David Michael Harper <66442656+DavidMichaelH@users.noreply.github.com>
2022-08-11 11:09:02 -0400

    added figure folder

--- project_traffic/last_passage_percolation/experiments/getting_started/getting_started.py
+++ project_traffic/last_passage_percolation/experiments/getting_started/getting_started.py
@@ -2,8 +2,8 @@
 #few things you need to do in order to get started.
 
 #The first thing is you need to copy and paste the file path to the root 
-#folder of this project last_passage_percolation
-#It should look something like C:/BLAH/BLAH/BLAH/last_passage_percolation"
+#folder that contains last_passage_percolation
+#It should look something like C:/BLAH/BLAH/BLAH/"
 FILE_PATH_TO_ROOT = "D:/project_traffic/"
  
 #Then just run the entire python file. This will tell your IDE where the root file of the project is to make all 
